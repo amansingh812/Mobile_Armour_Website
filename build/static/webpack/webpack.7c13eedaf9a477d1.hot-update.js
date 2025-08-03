2e5d77128fb41bb7d6eb0cbaa29e79d8b0bbf679
50064042 <140644142+50064042@users.noreply.github.com>
2025-08-03 21:38:11 +0530

    chore: update webpack build files and remove unused static assets fixed css

--- build/static/webpack/webpack.7c13eedaf9a477d1.hot-update.js
+++ build/static/webpack/webpack.7c13eedaf9a477d1.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ (() => {
-/******/ 	__webpack_require__.h = () => ("1fa5f798db8c395f")
+/******/ 	__webpack_require__.h = () => ("ade806c02b047dc1")
 /******/ })();
 /******/ 
 /******/ }
