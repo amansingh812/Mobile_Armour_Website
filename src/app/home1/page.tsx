2c7bcbf842dfe23321e6856a225fd39e59d04d49
amansingh812 <68216842+amansingh812@users.noreply.github.com>
2025-10-21 16:53:10 +0530

    style: adjust hero section heights, update slider image and add promo section padding

--- src/app/home1/page.tsx
+++ src/app/home1/page.tsx
@@ -11,14 +11,17 @@ import React from "react";
 
 import Hero1 from "@/components/sections/hero/hero1";
 import Hero2 from "@/components/sections/hero/hero2";
+import Hero4 from "@/components/sections/hero/hero4";
 
 function Home1({ }) {
   return (
     <div>
       <Hero1   />
       <Categories />
-      <About1 type="type-2" />
+      {/* <About1 type="type-2" /> */}
       <Hero2 />
+      <Hero4 />
+      
       <Service1 />
       <Promo1 />
     </div>
